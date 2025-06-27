da730f9026506ead777dda259a859e75e4256ffd
gladys <achandogladys6@gmail.com>
2025-06-27 18:06:26 +0300

    add new changes

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set();
+
+  for (const num of array) {
+    const complement = target - num;
+    if (seen.has(complement)) {
+      return true;
+    }
+    seen.add(num);
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time Complexity: O(n)
+  Space Complexity: O(n)
 */
 
 /* 
   Add your pseudocode here
+
+  - Create an empty Set to store numbers we've already seen.
+- Loop through each number in the array:
+  - Calculate the complement as target - current number
+  - If the complement is already in the Set, return true
+  - Otherwise, add the current number to the Set
+- If we finish the loop without finding a pair, return false
+
 */
 
 /*
   Add written explanation of your solution here
+  Explanation:
+  We use a Set to remember the numbers we've seen.
+  For each number, we check if its complement (target - number) exists in the Set.
+  If it does, we found two numbers that sum to the target.
+  If not, we keep checking.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +55,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([0, -1, 2, -3, 1], -2)); // -3 + 1
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([], 5));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([5], 5));
 }
 
 module.exports = hasTargetSum;
